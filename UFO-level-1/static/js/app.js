15cf71c6cea732e5b5e2acb7979e2626e6440913
RiverJAM <thetutorjoemonahan@gmail.com>
2020-12-11 09:48:21 -0600

    adds level one code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,6 +22,46 @@ data.forEach(function(aliens) {
   });
 });
 
-
 // Select the button
-var button = d3.select("filter-btn);
+var button = d3.select("#filter-btn");
+// Select the form
+var form = d3.select("form");
+// Create event handlers
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData);
+
+  var filteredData = tableData.filter((person) => person.datetime === inputValue);
+
+  console.log(filteredData);
+  
+  // Clear tbody
+  // Got this line from Tasha in slack line.
+  tbody.html("");
+ 
+  filteredData.forEach(function(aliens) {
+    
+    
+    var row = tbody.append("tr");
+  
+    Object.entries(aliens).forEach(function([key, value]) {
+      
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+}
